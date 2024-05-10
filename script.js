5796b39b76312e8f02cff40f0cb26fbc7cf18ca3
Marcos Euzébio <markeuzebio@gmail.com>
2024-05-10 09:46:42 -0300

    Register button on sidebar is now functional
    Everytime the button is pressed, a new card appears
    on the main part of the webpage.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn_show_hide_sidebar = document.querySelector('.btn-toggle-nav-sidebar');
-const nav_sidebar = document.querySelector('nav.sidebar');
-const body = document.querySelector('body');
+const nav_sidebar           = document.querySelector('nav.sidebar');
+const body                  = document.querySelector('body');
+const btn_register          = document.querySelector('button[name=btn_register]');
+const cards_container       = document.querySelector('.cards-container');
 
 btn_show_hide_sidebar.addEventListener('click', function () { 
     nav_sidebar.classList.toggle('invisible');
@@ -10,6 +12,33 @@ btn_show_hide_sidebar.addEventListener('click', function () {
         btn_show_hide_sidebar.textContent = '+';
     else
         btn_show_hide_sidebar.textContent = '-';
+});
+
+btn_register.addEventListener('click', function () {
+    const book_author   = document.querySelector('#book_author').value;
+    const book_title   = document.querySelector('#book_title').value;
+    const book_n_pages = document.querySelector('#book_n_pages').value;
+    const book_is_read = document.querySelector("input[type='radio'][name='read']:checked").value;
+
+    const card           = document.createElement('div');
+    card.classList.add('card');
+
+    const card_author  = document.createElement('p');
+    const card_title   = document.createElement('p');
+    const card_n_pages = document.createElement('p');
+    const card_is_read = document.createElement('p');
+
+    card_author.textContent = book_author;
+    card_title.textContent  = book_title;
+    card_n_pages.textContent  = book_n_pages;
+    card_is_read.textContent  = book_is_read;
+    
+    card.appendChild(card_author);
+    card.appendChild(card_title);
+    card.appendChild(card_n_pages);
+    card.appendChild(card_is_read);
+    
+    cards_container.appendChild(card);
 });
 
 const allBooks = [];
